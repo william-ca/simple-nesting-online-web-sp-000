ada8c7016e8faf3927ce733ef1f825041c8ec3ce
Will Andre <wandre94@gmail.com>
2021-01-08 17:51:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -102,10 +102,11 @@ def changing_alan
           :languages => ["C"]
         }
      }
-		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+		
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 
 end
 
